d969fa7bc2d8dabc50edb512f565615aa4c547e6
Floris van Dun <i489724@fhict.nl>
2023-04-20 15:09:11 +0200

    laatste verzie van de get translation pushen (niet gelukt deze uit te lezen.)

--- src/Components/About.js
+++ src/Components/About.js
@@ -45,6 +45,13 @@ export default function About() {
                 // setPost(data)
                 // posts.map((Bloks) => setPost(Bloks))
                 setTranslations(data);
+                // console.log(data);
+                // console.log(Translations);
+                console.log(Translations);
+                console.log(Translations);
+                console.log(Translations[1], "regel 50");
+                fakeArray[0] = Translations[1];
+                console.log(fakeArray[0], "regel 52")
             })
 
             .catch((err) =>
@@ -56,12 +63,19 @@ export default function About() {
      //const listTranslations = Translations.bloks.map((bloks,index) =>{
          //return (<div key={index}>bloks</div>);})
 
+    const fakeArray = [];
+    const secondFakeArray = [
+        ["email", "password"],
+        ["email2", "password2"]];
 
   return (
     <div>
         {/*{posts.map((bloks)=> {return bloks.map((Translation) => {return Translation.text})})}*/}
         {/*<button onClick={listTranslations}></button>*/}
-        {Translations.map((Translation) => {return Translation.text})}
+        {Object.keys(Translations).map((Translation, index) => {return(<div key={index}>{Translations[Translation].text}</div>)})}
+        {/*{fakeArray[0].map((a,b) => {return <li key={a + b}>{a}</li>; })}*/}
+        {/*{secondFakeArray.map((a,b = 1) => {return <li key={"hoi" + b}>{a}</li>; })}*/}
+        {/*{secondFakeArray[0]}*/}
       <h1>Over</h1>
     </div>
   );
