2abb234315ef94a170b4cb0360d1c60e08c1d867
Merel2002 <504227@student.fontys.nl>
2023-05-22 16:00:21 +0200

    UX eerste foutmelding

--- src/Components/RegisterStationCode.js
+++ src/Components/RegisterStationCode.js
@@ -4,6 +4,8 @@ import {wait} from "@testing-library/user-event/dist/utils";
 import '../index.css';
 
 export default function RegisterStationCode() {
+    const [errorMessage, setErrorMessage] = useState(null);
+
     const [registrationCode, setRegistrationCode] = useState();
     const [databaseTag, setDatabaseTag] = useState();
     let success = false;
@@ -12,18 +14,21 @@ export default function RegisterStationCode() {
             .then((response) => {
                 if(response.ok) {
                    success = true;
+                }else{
+                    setErrorMessage("De registratiecode bestaat niet...");
                 }
                 return response.text();
             })
             .then((data) => {
                 console.log(data);
             }).catch((err) => {
-                console.log(err.message);
+                setErrorMessage(err.message);
 
         });
     }
 
     const checkMeetstation = () => {
+        setErrorMessage(null);
         getMeetstationCode(databaseTag, registrationCode);
         wait(300).then(() => {
             if(success === true) {
@@ -35,6 +40,22 @@ export default function RegisterStationCode() {
             }}
         );
 
+    }
+
+    const buttonValidationCheck = () => {
+        let valid = false;
+
+        if(registrationCode != null && databaseTag != null){
+            valid = true;
+        }
+
+        if(valid == true){
+            return(
+            <Link to={"/station/create/name"} state={registrationCode}></Link>
+            )
+        }
+
+        return null;
     }
 
     const handleChangeCode = (event) => {
@@ -79,6 +100,13 @@ export default function RegisterStationCode() {
                             />
                         </div>
                     </div>
+                     <div className={"row"}>
+                         <div className={"col-4"}></div>
+                         <div className={"col-4"}>
+                             <br/>
+                             {errorMessage && <label className={"error-msg"}>{errorMessage}</label>}
+                         </div>
+                     </div>
                 </div>
                 <div className={"row mt-5"}>
                     <div className={"col-4"}></div>
@@ -88,8 +116,10 @@ export default function RegisterStationCode() {
                         </Link>
                         <button className={"button2"} onClick={checkMeetstation}>Checken</button>
                         <Link to={"/station/create/name"} state={registrationCode}>
-                            <button className={"button2"}>Volgende</button>
+                            <button className={"button2"} >Volgende</button>
                         </Link>
+
+                        {/*<button className={"button2"} onClick={buttonValidationCheck}>Volgende</button>*/}
                     </div>
                 </div>
             </div>
@@ -97,50 +127,3 @@ export default function RegisterStationCode() {
     );
 }
 
-
-// <br/>
-// <div className={"container gy-5"}>
-//     <div>
-//         <div className={"row"}>
-//             <div className={"col-4"}></div>
-//             <div className={"col-4"}>
-//                 <h4><b>(1/4) Meetstation toevoegen</b></h4>
-//                 <label>
-//                     <h5>Registratie code </h5>
-//                     <div className={"form-text"}> Registratie code is aanwezig op uw meetstation. </div>
-//                 </label>
-//
-//             </div>
-//         </div>
-//
-//         <div className={"row mt-1"}>
-//             <div className={"col-4"}></div>
-//             <div className={"col-2"}>
-//                 <select className={"form-select"} value={databaseTag} onChange={handleChangeTag} required>
-//                     <option selected={true}>Selecteer uw tag</option>
-//                     <option value={"MJS"}>MJS</option>
-//                 </select>
-//
-//             </div>
-//             <div className={"col-2"}>
-//                 <input type={"number"}
-//                        className={"form-control"}
-//                        placeholder={"Registratiecode..."}
-//                        onChange={handleChangeCode}
-//                        value={registrationCode}
-//                        required
-//                 />
-//             </div>
-//         </div>
-//     </div>
-//     <div className={"row mt-5"}>
-//         <div className={"col-4"}></div>
-//         <div className={"col-5"}>
-//             <button className={"btn btn-outline-primary mx-4"}>Annuleren</button>
-//             <button className={"btn btn-secondary mx-4"} onClick={checkMeetstation}>Checken</button>
-//             <Link to={"/station/create/name"} state={registrationCode}>
-//                 <button className={"btn btn-primary mx-4"}>Volgende</button>
-//             </Link>
-//         </div>
-//     </div>
-// </div>
