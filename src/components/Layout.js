b9c71597ce762466d5cd32e68e76d4412ba4400b
Rens van der Velden <I491926@fhict.nl>
2023-06-01 13:59:43 +0200

    Layout.js test

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
     return (
         <main className="App">
-            {/* represents all the children of the layout component, header and footer component can be added */}
+            {/* represents all the children of the layout component, header and footer component can be added! */}
             <Outlet/>
         </main>
     )
