323846360db341671e76a078e81033e37ac58134
Sander <74350276+886419@users.noreply.github.com>
2024-02-22 17:41:42 +0100

    semester-end (#87)
    * Initial legend
    
    * rectangels
    
    Bugfix/116 heatmap toggle
    
    * close enough for now
    
    -fieldnames parsed from JSON
    -radiobutton with variable options
    -start heatmap translater
    
    * struggles met .length
    
    -doorgeven van data gelukt
    
    * import removal
    
    -removed useEffect and useRef from heatmapLayer.js
    
    * slight cleanup
    
    -better naming {fieldname radio button}
    -removed console logs
    
    * heatmap working
    
    -heatmap is showing
    -updating heatmap not yet working
    
    * heatmap updates
    
    -heatmap updating added to heatmapLayer
    -FieldnameRadioButton upper case
    
    * radioButtonGroup back in home.js
    
    * last fixes and looks
    
    -RadioButtonGroup working again
    -FieldnameRadioButon dependent on RadioButtonGroup
    -indentation added to FieldnameRadioButoon
    
    * last tush
    
    -FieldNameRadioButoon remebers last clicked
    
    * unused and temporary buttons removed
    
    * Adds a Changelog for activities in s3nj23
    
    * Adds changelog template
    
    * Merged two components executing the same thing
    
    Also cleaned up code
    
    * Update to changelog
    
    * Adds maxDate
    
    * changed color pallete with preset values
    
    * Changed maxDate from datetime to just new Date()
    
    * console log removed
    
    * abstraction fix
    
    -if statement excludes fields instead of including them
    
    * Adds maxDate to meetstation kalenders
    
    * heatmap minimum set by code and fix in Fieldname Radio Button
    
    * Created initial legend
    
    Currently has hardcoded color values. Heatmp works, however the region layer still needs tweaking to reflect the colors.
    
    * Bug fixes after dev merge
    
    * default selected mode fix
    
    -fixed nothing being selected
    
    * Fixed start date cant be after end date
    
    * added several color pallete options, added minval logic
    
    * Added user interface for neighbourhood history
    
    * handeling NAN values in Meetstion
    
    * added handling for selecting the same date
    
    * Adds color-blind-pallete rainbow gradient
    
    * settled on final colours
    
    * removal of unused colours
    
    * onnodige comments weg en klueren heatmap en wijken het zelfden
    
    * Update CHANGELOG.md
    
    * Fixed error from merge conflict
    
    * Updated legend with general spectral colors
    
    * Error handling + loading message added
    
    * Changed Legend name to avoid conflicts with Legend Class in Recharts
    
    * change legend css names to prefix color
    
    * Updated changelog
    
    * Cleaned up changelog
    
    * Make neighbourhood data relevant to selected date
    
    * Changed url
    
    * changed center and zoom
    
    * changed colours and logic for heatmap and region layers
    
    * Update CHANGELOG.md
    
    * Update CHANGELOG version.md
    
    * opacity changes with history
    
    * change log updated
    
    * changelog and formatting
    
    * Fixed datepicker
    
    * comments added
    
    * Update ColorLegend.js comment
    
    * Adds Dockerfile.build and Dockerfile.release
    
    * deleted unnecessary components and pages
    
    * Updated color legend
    
    Legend now always contains the min and max value from the temps.
    
    * Updated changelog
    
    * title and icon changes
    
    * changelog
    
    * Docker stuff
    
    * Color legend turns invisble on humidity layer
    
    * project name changed in navbar
    
    * Update CHANGELOG.md
    
    * Updated changelog
    
    * corrected project name in navbar
    
    * Updated readme
    
    * Updated readme
    
    * max min fix
    
    * Moved nginx conf
    
    * updated deployment files
    
    ---------
    
    Co-authored-by: jakottelaar <jarno.kottelaar@gmail.com>
    Co-authored-by: Sdegraauw <144223601+Sdegraauw@users.noreply.github.com>
    Co-authored-by: bas <schipper.b@outlook.com>
    Co-authored-by: Niels Eppenhof <n.eppenhof@gmail.com>
    Co-authored-by: Lars-v-Delft <lars171998@gmail.com>
    Co-authored-by: Lars-v-Delft <144026980+Lars-v-Delft@users.noreply.github.com>
    Co-authored-by: jakottelaar <112403201+jakottelaar@users.noreply.github.com>

--- src/Components/HeatmapLayer.js
+++ src/Components/HeatmapLayer.js
@@ -1,81 +1,97 @@
 import { useMap } from 'react-leaflet';
 import HeatmapOverlay from 'leaflet-heatmap/leaflet-heatmap.js';
-import { useEffect, useRef } from 'react';
+import { spectralColors } from '../Lib/Utility.js';
 
-const HeatmapLayer = ({ data, visible }) => {
-    let maxTemp = 0;
+const HeatmapLayer = ({ data, visible, type }) => {
+    let maxval = Number.MIN_VALUE;
+    let minval = Number.MAX_VALUE;
+
     data.forEach(meting => {
-      if(meting.temperature > maxTemp) {
-        maxTemp = meting.temperature;
-      }
-    })
-
-    const changedDataFormat = {
-      max : maxTemp,
-      data : data.map(meting => {
-        return {
-          lat : meting.latitude,
-          lng : meting.longitude,
-          val : meting.temperature,
-          count : 1
+        if (meting[type]) {
+            if (meting[type] > maxval) {
+                maxval = meting[type];
+            }
+            if (meting[type] < minval) {
+                minval = meting[type];
+            }
         }
-      })
-    }
+    });
 
-    var radius_preview = 0.015;
-    var gradient_preview = {
-      '.05': 'blue',
-      '.4': 'green',
-      '.7': 'yellow',
-      '.9': 'red'
-    }
-    var radius_current = 0.0095
-    // Gradient color should taper off exponentially towards the bottom and top
-    var gradient_current = {
-      '.05': 'blue',
-      '.5': 'green',
-      '.6': 'yellow',
-      '.8': 'gold',
-      '.9': 'orange',
-      '.95': 'darkorange',
-      '.99': 'orangered',
-      '.995': 'red',
-      '1': 'firebrick'
-    }
+    //translates the relevant fields for the heatmap
+    const heatmapReadyData = [];
+    data.map(meting => {
+        if (meting[type]) {
+            heatmapReadyData.push(
+                {
+                    lat: meting.latitude,
+                    lng: meting.longitude,
+                    val: meting[type],
+                    count: 1
+                });
+        }
+    });
 
-    var tempConfig = {
-      // radius should be small ONLY if scaleRadius is true (or small radius is intended)
-      // if scaleRadius is false it will be the constant radius used in pixels
-      "radius": radius_preview,
-      "maxOpacity": .8,
-      // scales the radius based on map zoom
-      "scaleRadius": true,
-      // if set to false the heatmap uses the global maximum for colorization
-      // if activated: uses the data maximum within the current map boundaries
-      //   (there will always be a red spot with useLocalExtremas true)
-      "useLocalExtrema": false,
-      // enter n keys between 0 and 1 here for gradient color customization
-      "gradient": gradient_current,
-      // which field name in your data represents the latitude - default "lat"
-      latField: 'lat',
-      // which field name in your data represents the longitude - default "lng"
-      lngField: 'lng',
-      // which field name in your data represents the data value - default "value"
-      valueField: 'val'
+    //loads data into final object for heatmap
+    data = {
+        max: maxval,
+        min: minval - ((maxval - minval) * 0.5),
+        data: heatmapReadyData
     };
-    
+
+    const radius_preview = 0.015;
+    // Gradient color should taper off exponentially towards the bottom and top
+    const rainbow_gradient = {
+        '.20': spectralColors[0],
+        '.32': spectralColors[1],
+        '.44': spectralColors[2],
+        '.56': spectralColors[3],
+        '.68': spectralColors[4],
+        '.77': spectralColors[5],
+        '.84': spectralColors[6],
+        '.90': spectralColors[7],
+        '.95': spectralColors[8],
+        '1': spectralColors[9]
+    }
+
+    const tempConfig = {
+        // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+        // if scaleRadius is false it will be the constant radius used in pixels
+        "radius": radius_preview,
+        "maxOpacity": .85,
+        "minOpacity": .08,
+        // scales the radius based on map zoom
+        "scaleRadius": true,
+        // if set to false the heatmap uses the global maximum for colorization
+        // if activated: uses the data maximum within the current map boundaries
+        //   (there will always be a red spot with useLocalExtremas true)
+        "useLocalExtrema": false,
+        // enter n keys between 0 and 1 here for gradient color customization
+        "gradient": rainbow_gradient,
+        // which field name in your data represents the latitude - default "lat"
+        latField: 'lat',
+        // which field name in your data represents the longitude - default "lng"
+        lngField: 'lng',
+        // which field name in your data represents the data value - default "value"
+        valueField: 'val',
+    };
+
     const map = useMap();
     const heatmapLayer = new HeatmapOverlay(tempConfig);
-    heatmapLayer.setData(changedDataFormat);
+    heatmapLayer.setData(data);
 
     if (visible) {
-      map.addLayer(heatmapLayer);
+        //looks for existing heatmap to update
+        map.eachLayer(function (layer) {
+            if (layer._heatmap)
+                layer.remove();
+        })
+        map.addLayer(heatmapLayer);
     }
-    if (!visible) {
-      map.eachLayer(function (layer) {
-        if (layer._heatmap)
-          layer.remove();
-      })
+    else {
+        map.eachLayer(function (layer) {
+            if (layer._heatmap)
+                layer.remove();
+        })
     }
 }
 
