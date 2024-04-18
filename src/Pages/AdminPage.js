25747a3cf1180ae2b4edcb0a120a9ecd7dd99cc6
Ding,Amy A.X.Y. <a.ding@student.fontys.nl>
2024-04-18 10:59:34 +0200

    Remove unused code

--- src/Pages/AdminPage.js
+++ src/Pages/AdminPage.js
@@ -3,51 +3,24 @@ import useAuth from '../Hooks/useAuth';
 import { Link } from 'react-router-dom';
 import { api } from "../App";
 
-const ADMIN_URL = '/Admin/newworkshopcode';
+const ADMIN_URL = '/Admin/createworkshopcode';
 
-
-function GenerateRandomCode(length) {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    let randomCode = '';
-    for (let i = 0; i < length; i++) {
-         let newChar = characters.charAt(Math.floor(Math.random() * characters.length));
-         randomCode += newChar;
-         console.log(newChar)
-    }
-    console.log(randomCode)
-
-    return randomCode;
-}
 const AdminPage = () => {
     const [workshopCode, setWorkshopCode] = useState('')
     const [duration, setDuration] = useState(10)
-    const [currentDateTime, setCurrentDateTime] = useState(new Date())
-    const [expDateTime, setExpDateTime] = useState(new Date())
+    // const [currentDateTime, setCurrentDateTime] = useState(new Date())
+    // const [expDateTime, setExpDateTime] = useState(new Date())
     const [length, setLength] = useState(6)
 
-    useEffect(() => {
-        setCurrentDateTime(new Date());
-        const newExpDateTime = currentDateTime;
-        newExpDateTime.setMinutes(currentDateTime.getMinutes() + duration);
-        setExpDateTime(newExpDateTime);
-    }, [duration, length]);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        setWorkshopCode(GenerateRandomCode(length));
-        const item = {workshopCode, expDateTime}
 
         try {
-            const response = await api.post(ADMIN_URL, JSON.stringify({
-                    // firstName: firstname,
-                    // lastName: surname,
-                    // password: password,
-                    // confirmPassword: confirmPassword,
-                    // mailAddress: email
-                }),
+            const response = await api.get(ADMIN_URL, JSON.stringify({ duration: duration, length: length }),
                 {
                     headers: {'Content-Type': 'application/JSON'},
-                    withCredentials: false
+                    withCredentials: true
                 });
 
             console.log(JSON.stringify(response?.data));
