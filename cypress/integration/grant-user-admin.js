319b8210dd8b4f097adb9e67f93e63db7ccf7dcc
Ayuub <519818@student.fontys.nl>
2024-05-15 10:40:29 +0200

    Update grant-user-admin.js
    Cypress now tests if the user actually got admin and actually lost admin

--- cypress/integration/grant-user-admin.js
+++ cypress/integration/grant-user-admin.js
@@ -1,4 +1,4 @@
-describe('Registration Form Tests', () => {
+describe('Test empty fields', () => {
     beforeEach(() => {
         cy.setCookie('user-id', '34');
         cy.visit('/Admin/grantUserAdmin');
@@ -21,5 +21,30 @@ describe('Registration Form Tests', () => {
         cy.get('#selectAdminRightsDropdown').select('');
         cy.get('#submitbutton').contains('Volgende').click();
         cy.get('.error-msg').should('contain', 'Please fill in every field');
+    });
+});
+
+describe('Test admin', () => {
+    beforeEach(() => {
+        cy.setCookie('user-id', '34');
+        cy.visit('/Admin/grantUserAdmin');
+    });
+
+    it('user 1 should get admin rights', () => {
+        cy.get('#selectUserDropdown').select(1);
+        cy.get('#selectAdminRightsDropdown').select('true');
+        cy.get('#submitbutton').contains('Volgende').click();
+        cy.setCookie('user-id', '1');
+        cy.visit('/Admin/grantUserAdmin');
+        cy.get('h4').should('contain', 'Geef een gebruiker administrator rechten');
+    });
+
+    it('user 1 should lose admin rights', () => {
+        cy.get('#selectUserDropdown').select(1);
+        cy.get('#selectAdminRightsDropdown').select('false');
+        cy.get('#submitbutton').contains('Volgende').click();
+        cy.setCookie('user-id', '1');
+        cy.visit('/Admin/grantUserAdmin');
+        cy.get('h1').should('contain', 'Nuh uh');
     });
 });
