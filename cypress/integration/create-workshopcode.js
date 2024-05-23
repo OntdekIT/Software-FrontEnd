3469710d3ee27f8852ea95eafcb5ba36fac473e4
Ding,Amy A.X.Y. <a.ding@student.fontys.nl>
2024-05-23 14:20:41 +0200

    Update createworkshopcode end to end test

--- cypress/integration/create-workshopcode.js
+++ cypress/integration/create-workshopcode.js
@@ -11,30 +11,25 @@ describe("Register station page tests", () => {
         const code = '';
 
         cy.visit("/Admin/workshopcode/create");
-        cy.intercept("POST", "http://localhost:8082/api/Admin/workshopcode/create", {}).as(
-            "createWorkshopcode"
-        );
 
-        wait(5000);
+        wait(10000);
 
         cy.get('.col-2 select').eq(0).select('4 uur');
         cy.get('.col-2 select').eq(1).select('6 cijfers');
 
         cy.get('.button2').click();
 
+        cy.intercept('POST', '/Admin/workshopcode/create', (req) => {}).as(
+            'codeRequest'
+            );
 
-        cy.intercept("POST", "http://localhost:3000/Admin/workshopcode/show", {}).as(
-            "showWorkshopcode"
-        );
-
-        cy.get("@showWorkshopcode").then((interception) => {
+        cy.get("@codeRequest").then((interception) => {
             // Get the value from localStorage
             const workshopcode = localStorage.getItem('workshopcode');
 
             // Ensure random string has correct length
             expect(workshopcode).to.have.lengthOf(length);
         });
-
 
     });
 });
