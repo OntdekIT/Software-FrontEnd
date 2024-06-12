3392c90741d927f1ea2f58f40b42a3cf0b1199b4
Jesse Kraus <93532284+KrausJ2002@users.noreply.github.com>
2024-06-12 14:28:16 +0200

    double verify email fix
    Pressing the login or register button multiple times wont sent multiple emails anymore.

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,93 +1,98 @@
-import { useRef, useState, useEffect } from "react";
+import {useRef, useState, useEffect} from "react";
 import useAuth from '../Hooks/useAuth';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import Verify from "../Components/Verify";
-import { api } from "../App";
+import {api} from "../App";
 
 const LOGIN_URL = '/Authentication/login';
 
 const Login = () => {
-  const { setAuth } = useAuth();
-  const mailRef = useRef();
-  const errRef = useRef();
-  const passwordRef = useRef();
+    const {setAuth} = useAuth();
+    const mailRef = useRef();
+    const errRef = useRef();
+    const passwordRef = useRef();
 
-  const [mail, setMail] = useState('');
-  const [errMsg, setErrMsg] = useState('');
-  const [password, setPassword] = useState('');
-  const [verify, setVerify] = useState(false);
+    const [mail, setMail] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const [password, setPassword] = useState('');
+    const [verify, setVerify] = useState(false);
+    const [isSubmitting, setIsSubmitting] = useState(false); // State to manage form submission status
 
-  useEffect(() => {
-    mailRef.current.focus();
-  }, []);
+    useEffect(() => {
+        mailRef.current.focus();
+    }, []);
 
-  useEffect(() => {
-    setErrMsg('');
-  }, [mail]);
+    useEffect(() => {
+        setErrMsg('');
+    }, [mail]);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await api.post(LOGIN_URL, JSON.stringify({ mailAddress: mail, password: password }), {
-        headers: { 'Content-Type': 'application/JSON' },
-        withCredentials: false
-      });
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setIsSubmitting(true); // Disable form on submit
+        try {
+            const response = await api.post(LOGIN_URL, JSON.stringify({mailAddress: mail, password: password}), {
+                headers: {'Content-Type': 'application/JSON'},
+                withCredentials: false
+            });
 
-      const accessToken = response?.data?.accessToken;
-      const roles = response?.data?.roles;
-      setAuth({ mail, password, roles, accessToken });
+            const accessToken = response?.data?.accessToken;
+            const roles = response?.data?.roles;
+            setAuth({mail, password, roles, accessToken});
 
-      if (response?.status === 200) {
-        setVerify(true);
-      }
-    } catch (err) {
-
+            if (response?.status === 200) {
+                setVerify(true);
+            }
+        } catch (err) {
+            setErrMsg('Login failed');
+            setIsSubmitting(false); // Re-enable form if there's an error
+        }
     }
-  }
 
-  return (
-      <section className="form-section">
-        <title>Login</title>
-        {errMsg && <div ref={errRef} className="error-msg">{errMsg}</div>}
+    return (
+        <section className="form-section">
+            <title>Login</title>
+            {errMsg && <div ref={errRef} className="error-msg">{errMsg}</div>}
 
-        {verify ? (
-            <Verify mail={mail} />
-        ) : (
-            <div>
-              <h1>Inloggen</h1>
-              <form onSubmit={handleSubmit}>
-                <input
-                    type="email"
-                    id="email"
-                    ref={mailRef}
-                    autoComplete="off"
-                    onChange={(e) => setMail(e.target.value)}
-                    value={mail}
-                    required
-                    placeholder="Email"
-                />
-                <input
-                    type="password"
-                    id="password"
-                    ref={passwordRef}
-                    autoComplete="off"
-                    onChange={(e) => setPassword(e.target.value)}
-                    value={password}
-                    required
-                    placeholder="Password"
-                />
-                <button className="button">Inloggen</button>
-              </form>
-              <div className="form-redirect">
-                <p>Nog geen account?</p>
-                <span className="line">
+            {verify ? (
+                <Verify mail={mail}/>
+            ) : (
+                <div>
+                    <h1>Inloggen</h1>
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            type="email"
+                            id="email"
+                            ref={mailRef}
+                            autoComplete="off"
+                            onChange={(e) => setMail(e.target.value)}
+                            value={mail}
+                            required
+                            placeholder="Email"
+                            disabled={isSubmitting} // Disable input when submitting
+                        />
+                        <input
+                            type="password"
+                            id="password"
+                            ref={passwordRef}
+                            autoComplete="off"
+                            onChange={(e) => setPassword(e.target.value)}
+                            value={password}
+                            required
+                            placeholder="Password"
+                            disabled={isSubmitting} // Disable input when submitting
+                        />
+                        <button className="button" disabled={isSubmitting}>Inloggen</button>
+                    </form>
+                    <div className="form-redirect">
+                        <p>Nog geen account?</p>
+                        <span className="line">
               <Link to="/register">Registreren</Link>
             </span>
-              </div>
-            </div>
-        )}
-      </section>
-  )
+                    </div>
+                </div>
+            )}
+        </section>
+    )
 }
 
 export default Login;
--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,177 +1,195 @@
-import { useRef, useState, useEffect } from "react";
+import {useRef, useState, useEffect} from "react";
 import useAuth from '../Hooks/useAuth';
-import { Link } from 'react-router-dom';
-import { api } from "../App";
+import {Link} from 'react-router-dom';
+import {api} from "../App";
 import Verify from "../Components/Verify";
 
 const REGISTER_URL = '/Authentication/register';
 
 const Register = () => {
-  const { setAuth } = useAuth();
-  const firstnameRef = useRef();
-  const surnameRef = useRef();
-  const passwordRef = useRef();
-  const confirmPasswordRef = useRef();
-  const emailRef = useRef();
-  const meetstationCodeRef = useRef();
-  const workshopCodeRef = useRef();
-  const errRef = useRef();
-  const successRef = useRef();
+    const {setAuth} = useAuth();
+    const firstnameRef = useRef();
+    const surnameRef = useRef();
+    const passwordRef = useRef();
+    const confirmPasswordRef = useRef();
+    const emailRef = useRef();
+    const meetstationCodeRef = useRef();
+    const workshopCodeRef = useRef();
+    const errRef = useRef();
+    const successRef = useRef();
 
-  const [firstname, setFirstname] = useState('');
-  const [surname, setSurname] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [email, setEmail] = useState('');
-  const [meetstationCode, setMeetstationCode] = useState('');
-  const [workshopCode, setWorkshopCode] = useState('');
-  const [errMsg, setErrMsg] = useState('');
-  const [successMsg, setSuccessMsg] = useState('');
-  const [verify, setVerify] = useState(false); // Add verify state
+    const [firstname, setFirstname] = useState('');
+    const [surname, setSurname] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [meetstationCode, setMeetstationCode] = useState('');
+    const [workshopCode, setWorkshopCode] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const [successMsg, setSuccessMsg] = useState('');
+    const [verify, setVerify] = useState(false); // Add verify state
+    const [isSubmitting, setIsSubmitting] = useState(false); // State to manage form submission status
 
-  useEffect(() => {
-    firstnameRef.current.focus();
-  }, []);
+    useEffect(() => {
+        firstnameRef.current.focus();
+    }, []);
 
-  useEffect(() => {
-    setErrMsg('');
-  }, [firstname, surname, password, confirmPassword, email, meetstationCode, workshopCode])
+    useEffect(() => {
+        setErrMsg('');
+    }, [firstname, surname, password, confirmPassword, email, meetstationCode, workshopCode])
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setIsSubmitting(true); // Disable form on submit
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+        try {
+            const response = await api.post(REGISTER_URL, JSON.stringify({
+                    firstName: firstname,
+                    lastName: surname,
+                    password: password,
+                    confirmPassword: confirmPassword,
+                    mailAddress: email,
+                    meetstationCode: meetstationCode,
+                    workshopCode: workshopCode,
+                }),
+                {
+                    headers: {'Content-Type': 'application/JSON'},
+                    withCredentials: false
+                });
 
-    try {
-      const response = await api.post(REGISTER_URL, JSON.stringify({
-            firstName: firstname,
-            lastName: surname,
-            password: password,
-            confirmPassword: confirmPassword,
-            mailAddress: email,
-            meetstationCode: meetstationCode,
-            workshopCode: workshopCode,
-          }),
-        {
-          headers: { 'Content-Type': 'application/JSON' },
-          withCredentials: false
-        });
+            const accessToken = response?.data?.accessToken;
+            const roles = response?.data?.roles;
 
-      const accessToken = response?.data?.accessToken;
-      const roles = response?.data?.roles;
+            //save all of our info in auth object, which is saved in global context
+            setAuth({
+                firstname,
+                surname,
+                password,
+                confirmPassword,
+                email,
+                meetstationCode,
+                workshopCode,
+                roles,
+                accessToken
+            });
 
-      //save all of our info in auth object, which is saved in global context
-      setAuth({ firstname, surname, password, confirmPassword, email, meetstationCode, workshopCode, roles, accessToken });
+            if (response?.status === 201) {
+                setVerify(true);
+            }
+        } catch (err) {
+            setIsSubmitting(false); // Re-enable form if there's an error
+            if (!err?.response) {
+                setErrMsg('Kon geen verbinding maken, probeer het later opnieuw');
+            } else {
+                setErrMsg(err.response.data);
 
-      if (response?.status === 201) {
-        setVerify(true);
-      }
-    } catch (err) {
-      if (!err?.response) {
-        setErrMsg('Kon geen verbinding maken, probeer het later opnieuw');
+                console.log(err);
+                console.log(err.response);
+                console.log(err.errMsg);
+            }
         }
-      else {
-        setErrMsg(err.response.data);
-        
-        console.log(err);
-        console.log(err.response);
-        console.log(err.errMsg);
-      }
     }
-  }
 
-  return (
-      <section className="form-section">
-        <title>Register</title>
-        {errMsg && <div ref={errRef} className="error-msg">{errMsg}</div>}
-        {successMsg && <div ref={successRef} className="success-msg">{successMsg}</div>}
-        {verify ? (
-            <Verify mail={email} />
-        ) : (
-            <div>
-              <h1>Registreren</h1>
-              <form onSubmit={handleSubmit}>
-                <label htmlFor="firstname">First name</label>
-                <input
-                    type="text"
-                    id="firstname"
-                    ref={firstnameRef}
-                    autoComplete="off"
-                    onChange={(e) => setFirstname(e.target.value)}
-                    value={firstname}
-                    placeholder="Voornaam"
-                />
-                <label htmlFor="surname">Last name</label>
-                <input
-                    type="text"
-                    id="surname"
-                    ref={surnameRef}
-                    autoComplete="off"
-                    onChange={(e) => setSurname(e.target.value)}
-                    value={surname}
-                    placeholder="Achternaam"
-                />
-                <label htmlFor="email">Email</label>
-                <input
-                    type="text"
-                    id="email"
-                    ref={emailRef}
-                    autoComplete="off"
-                    onChange={(e) => setEmail(e.target.value)}
-                    value={email}
-                    placeholder="Email"
-                />
-                <label htmlFor="password">Password</label>
-                <input
-                    type="password"
-                    id="password"
-                    ref={passwordRef}
-                    autoComplete="off"
-                    onChange={(e) => setPassword(e.target.value)}
-                    value={password}
-                    placeholder="Wachtwoord"
-                />
-                <label htmlFor="confirmPassword">Confirm Password</label>
-                <input
-                    type="password"
-                    id="confirmPassword"
-                    ref={confirmPasswordRef}
-                    autoComplete="off"
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                    value={confirmPassword}
-                    placeholder="Herhaal Wachtwoord"
-                />
-                <label htmlFor="meetstationCode">Meetstation Code</label>
-                <input
-                    type="number"
-                    id="meetstationCode"
-                    ref={meetstationCodeRef}
-                    autoComplete="off"
-                    onChange={(e) => setMeetstationCode(e.target.value)}
-                    value={meetstationCode}
-                    placeholder="123456"
-                />
-                <label htmlFor="workshopCode">Workshop Code</label>
-                <input
-                    type="number"
-                    id="workshopCode"
-                    ref={workshopCodeRef}
-                    autoComplete="off"
-                    onChange={(e) => setWorkshopCode(e.target.value)}
-                    value={workshopCode}
-                    placeholder="123456"
-                />
-                <button className="button">Registreren</button>
-              </form>
-              <div className="form-redirect">
-                <p>Al een account?</p>
-                <span className="line">
-              <Link to="/login">Inloggen</Link>
-            </span>
-              </div>
-            </div>
-        )}
-      </section>
-  )
+    return (
+        <section className="form-section">
+            <title>Register</title>
+            {errMsg && <div ref={errRef} className="error-msg">{errMsg}</div>}
+            {successMsg && <div ref={successRef} className="success-msg">{successMsg}</div>}
+            {verify ? (
+                <Verify mail={email}/>
+            ) : (
+                <div>
+                    <h1>Registreren</h1>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="firstname">First name</label>
+                        <input
+                            type="text"
+                            id="firstname"
+                            ref={firstnameRef}
+                            autoComplete="off"
+                            onChange={(e) => setFirstname(e.target.value)}
+                            value={firstname}
+                            placeholder="Voornaam"
+                            disabled={isSubmitting} // Disable input when submitting
+                        />
+                        <label htmlFor="surname">Last name</label>
+                        <input
+                            type="text"
+                            id="surname"
+                            ref={surnameRef}
+                            autoComplete="off"
+                            onChange={(e) => setSurname(e.target.value)}
+                            value={surname}
+                            placeholder="Achternaam"
+                            disabled={isSubmitting} // Disable input when submitting
+                        />
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="text"
+                            id="email"
+                            ref={emailRef}
+                            autoComplete="off"
+                            onChange={(e) => setEmail(e.target.value)}
+                            value={email}
+                            placeholder="Email"
+                            disabled={isSubmitting} // Disable input when submitting
+                        />
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            ref={passwordRef}
+                            autoComplete="off"
+                            onChange={(e) => setPassword(e.target.value)}
+                            value={password}
+                            placeholder="Wachtwoord"
+                            disabled={isSubmitting} // Disable input when submitting
+                        />
+                        <label htmlFor="confirmPassword">Confirm Password</label>
+                        <input
+                            type="password"
+                            id="confirmPassword"
+                            ref={confirmPasswordRef}
+                            autoComplete="off"
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                            value={confirmPassword}
+                            placeholder="Herhaal Wachtwoord"
+                            disabled={isSubmitting} // Disable input when submitting
+                        />
+                        <label htmlFor="meetstationCode">Meetstation Code</label>
+                        <input
+                            type="number"
+                            id="meetstationCode"
+                            ref={meetstationCodeRef}
+                            autoComplete="off"
+                            onChange={(e) => setMeetstationCode(e.target.value)}
+                            value={meetstationCode}
+                            placeholder="123456"
+                            disabled={isSubmitting} // Disable input when submitting
+                        />
+                        <label htmlFor="workshopCode">Workshop Code</label>
+                        <input
+                            type="number"
+                            id="workshopCode"
+                            ref={workshopCodeRef}
+                            autoComplete="off"
+                            onChange={(e) => setWorkshopCode(e.target.value)}
+                            value={workshopCode}
+                            placeholder="123456"
+                            disabled={isSubmitting} // Disable input when submitting
+                        />
+                        <button className="button" disabled={isSubmitting}>Registreren</button>
+                    </form>
+                    <div className="form-redirect">
+                        <p>Al een account?</p>
+                        <span className="line">
+                  <Link to="/login">Inloggen</Link>
+                </span>
+                    </div>
+                </div>
+            )}
+        </section>
+    )
 }
 
 export default Register;
