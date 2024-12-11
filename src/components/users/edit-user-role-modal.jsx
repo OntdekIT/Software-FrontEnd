0127e9eb8076b9e638e7d74164e0cae8b4cc2c24
Arjan Gosens <a.gosens@student.fontys.nl>
2024-12-11 09:21:38 +0100

    Modified EditUserRoleModal to only show allowed roles

--- src/components/users/edit-user-role-modal.jsx
+++ src/components/users/edit-user-role-modal.jsx
@@ -1,17 +1,37 @@
 import {Button, Modal} from "react-bootstrap";
 import {backendApi} from "../../utils/backend-api.jsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import PropTypes from "prop-types";
 import UserRole from "../../domain/user-role.jsx";
 import UserUtils from "../../utils/user-utils.jsx";
 import {useForm} from "react-hook-form";
+import {useAuth} from "../../providers/auth-provider.jsx";
 
 export default function EditUserRoleModal({user, isShown, onClose, onRoleChanged}) {
+    const {loggedInUser} = useAuth();
+    const [roleOptions, setRoleOptions] = useState([]);
     const [isSubmitProcessing, setIsSubmitProcessing] = useState(false);
     const [error, setError] = useState(null);
     const {register, handleSubmit, formState: {errors}} = useForm({
         mode: "onBlur"
     });
+
+    const calculateRoleOptions = () => {
+        const loggedInUserRole = loggedInUser.role;
+        const userRole = user.role;
+
+        if (loggedInUserRole === UserRole.SUPER_ADMIN) {
+            setRoleOptions([UserRole.USER, UserRole.ADMIN, UserRole.SUPER_ADMIN]);
+        } else if (loggedInUserRole === UserRole.ADMIN) {
+            if (userRole === UserRole.USER) {
+                setRoleOptions([UserRole.USER, UserRole.ADMIN]);
+            } else if (userRole === UserRole.ADMIN) {
+                setRoleOptions([UserRole.ADMIN]);
+            } else {
+                setRoleOptions([UserRole.SUPER_ADMIN]);
+            }
+        }
+    };
 
     const handleUpdateRole = async (value) => {
         setIsSubmitProcessing(true);
@@ -38,6 +58,10 @@ export default function EditUserRoleModal({user, isShown, onClose, onRoleChanged
         }
     };
 
+    useEffect(() => {
+        calculateRoleOptions();
+    }, [loggedInUser, user]);
+
     return (
         <Modal show={isShown} onHide={onClose}>
             <form onSubmit={handleSubmit(handleUpdateRole)}>
@@ -48,7 +72,8 @@ export default function EditUserRoleModal({user, isShown, onClose, onRoleChanged
                     <div>
                         {error && <div className="error-msg">{error}</div>}
                     </div>
-                    <p>Selecteer een rol om aan de gebruiker <b>{`${user.firstName} ${user.lastName}`}</b> toe te kennen.</p>
+                    <p>Selecteer een rol om aan de gebruiker <b>{`${user.firstName} ${user.lastName}`}</b> toe te
+                        kennen.</p>
                     <label>Rol<small className="text-danger">*</small></label>
                     <select
                         className={"form-select" + (errors.role ? " is-invalid" : "")}
@@ -56,9 +81,9 @@ export default function EditUserRoleModal({user, isShown, onClose, onRoleChanged
                         defaultValue={user.role}
                         disabled={isSubmitProcessing}
                     >
-                        <option value={UserRole.USER}>{UserUtils.translateRole(UserRole.USER)}</option>
-                        <option value={UserRole.ADMIN}>{UserUtils.translateRole(UserRole.ADMIN)}</option>
-                        <option value={UserRole.SUPER_ADMIN}>{UserUtils.translateRole(UserRole.SUPER_ADMIN)}</option>
+                        {roleOptions.map(role => (
+                            <option key={role} value={role}>{UserUtils.translateRole(role)}</option>
+                        ))}
                     </select>
                     {errors.role && <div className="invalid-feedback">{errors.role.message}</div>}
                 </Modal.Body>
